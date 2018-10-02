539e9d2dc12b2fcea3346331267d1ff3f69c943b
Karen Kuek <karenkk75@gmail.com>
2018-10-02 16:01:41 +1000

    planning and simple 3x3 grid done

---
+++ app.js
@@ -0,0 +1,10 @@
+// create a 9-box grid (css ? flex-box) -> do it in 9 individual boxes OR just one whole grid (can that be done??) ==> COMPLETED using flex-box
+// create "X"s and "O"s -> onclick
+// 2 players taking turns each -> how? create 2 separate functions? but then would need to link them back together?
+// OR print "X" on first click, "O" on 2nd click and alternate? - display that it is player 1 or 2's turn clearly
+// create the winning scenarios -> win in a straight row OR win in a diagonal row
+
+// Find pattern:
+// create ONE box which can have "X" or "O" onclick, BUT cannot be clicked again or changed on another click
+// repeat pattern x 8 more times to create the board of 3x3 grid
+
